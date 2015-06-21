4bb6dbd3d03fe344fd81ece871b85cda2f3bf141
Jason Stathopulos <jstathopulos@gmail.com>
2015-06-21 17:51:53 -0400

    Add Routes Going To Be Used In Application
    Add in the necessary routes to the components which will load when the
    user goes to that paging using a particular URL.

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+var React = require('react');
+var Router = require('react-router');
+
+var Route = Router.Route;
+var DefaultRoute = Router.DefaultRoute;
+
+var App = 			require('./components/App.react.jsx');
+var TodosPage = require('./components/todos/TodosPage.react.jsx');
+var TodoPage = 	require('./components/todos/TodoPage.react.jsx');
+var TodosNew = 	require('./components/todos/TodosNew.react.jsx');
+
+module.exports = (
+  <Route name="app" path="/" handler={App}>
+    <DefaultRoute handler={TodosPage} />
+    <Route name="todos" path="/todos" handler={TodosPage}/>
+    <Route name="Todo" path="/todos/:todoid" handler={TodoPage} />
+    <Route name="new-todo" path="/todo/new" handler={TodoNew}/>
+  </Route>
+);
