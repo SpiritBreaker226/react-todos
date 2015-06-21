4977ae9a197ca2aa879e1998f868f7be7bb3cc45
Jason Stathopulos <jstathopulos@gmail.com>
2015-06-21 16:21:43 -0400

    Remove getInitialState From Main Component
    Remove getInitialState from Main component because it does not belong in
    the main component since the Todos will be hold the data it should be
    part of the Flux architecture.

--- src/components/ReactTodosApp.js
+++ src/components/ReactTodosApp.js
@@ -10,11 +10,6 @@ require('../styles/main.css');
 var imageURL = require('../images/yeoman.png');
 
 var ReactTodosApp = React.createClass({
-	getInitialState: function() {
-		return {
-			todos: []
-		}
-	},
 	render: function() {
 		return (
 			<main role="main" className="container">
