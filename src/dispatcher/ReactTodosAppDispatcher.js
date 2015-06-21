78e2a0625aaa5b19d541429b5cfdf06fd6fa4adc
Jason Stathopulos <jstathopulos@gmail.com>
2015-06-21 17:04:51 -0400

    Add Function handleViewAction To Dispatcher Object
    Add the function handleViewAction to Dispatcher object because this
    function will be used to handle various actions in the view/competent.

--- src/dispatcher/ReactTodosAppDispatcher.js
+++ src/dispatcher/ReactTodosAppDispatcher.js
@@ -1,3 +1,18 @@
 var Dispatcher = require('flux').Dispatcher;
+var assign = require('object-assign');
 
-module.exports = new Dispatcher();
+var Constants = require('../constants.js');
+
+var PayloadSources = Constants.PayloadSources;
+
+var Dispatcher = assign(new Dispatcher(), {
+	handleViewAction: function(action) {
+    var payload = {
+      source: PayloadSources.VIEW_ACTION,
+      action: action
+    };
+    this.dispatch(payload);
+  }
+});
+
+module.exports = Dispatcher;
