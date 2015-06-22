92ab424b841138cbdaa3a7243f24b44f433fe7b9
Jason Stathopulos <jstathopulos@gmail.com>
2015-06-21 21:22:04 -0400

    Remove Routes Currently Not Being Used

--- src/routes.js
+++ src/routes.js
@@ -6,16 +6,9 @@ var Router = require('react-router');
 var Route = Router.Route;
 var DefaultRoute = Router.DefaultRoute;
 
-var ReactTodosApp = require('./components/ReactTodosApp.react.jsx');
-var TodosPage = 		require('./components/todos/TodosPage.react.jsx');
-var TodoPage = 			require('./components/todos/TodoPage.react.jsx');
-var TodosNew = 			require('./components/todos/TodosNew.react.jsx');
+var ReactTodosApp = require('./components/ReactTodosApp.js');
 
 module.exports = (
   <Route name="app" path="/" handler={ReactTodosApp}>
-    <DefaultRoute handler={TodosPage} />
-    <Route name="todos" path="/todos" handler={TodosPage}/>
-    <Route name="Todo" path="/todos/:todoid" handler={TodoPage} />
-    <Route name="new-todo" path="/todo/new" handler={TodoNew}/>
   </Route>
 );
