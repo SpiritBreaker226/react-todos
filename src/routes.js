9ca485a9f1b6aaebf882c1611bbd676fbbcc5ec4
Jason Stathopulos <jstathopulos@gmail.com>
2015-06-21 21:05:12 -0400

    Rename App To ReactTodosApp In Routes
    Rename App to ReactTodosApp because in React the main Application should
    be name of the application with App at the end of it.

--- src/routes.js
+++ src/routes.js
@@ -6,13 +6,13 @@ var Router = require('react-router');
 var Route = Router.Route;
 var DefaultRoute = Router.DefaultRoute;
 
-var App = 			require('./components/App.react.jsx');
-var TodosPage = require('./components/todos/TodosPage.react.jsx');
-var TodoPage = 	require('./components/todos/TodoPage.react.jsx');
-var TodosNew = 	require('./components/todos/TodosNew.react.jsx');
+var ReactTodosApp = require('./components/ReactTodosApp.react.jsx');
+var TodosPage = 		require('./components/todos/TodosPage.react.jsx');
+var TodoPage = 			require('./components/todos/TodoPage.react.jsx');
+var TodosNew = 			require('./components/todos/TodosNew.react.jsx');
 
 module.exports = (
-  <Route name="app" path="/" handler={App}>
+  <Route name="app" path="/" handler={ReactTodosApp}>
     <DefaultRoute handler={TodosPage} />
     <Route name="todos" path="/todos" handler={TodosPage}/>
     <Route name="Todo" path="/todos/:todoid" handler={TodoPage} />
