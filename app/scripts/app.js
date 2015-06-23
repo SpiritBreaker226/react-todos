de5289e4b51a2103639641aab61c8ba1f49ef632
Jason Stathopulos <jstathopulos@gmail.com>
2015-06-23 13:25:57 -0400

    Add Todo Form To Create New Items

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,13 @@
 var React = window.React = require('react');
 var TodoList = require('./components/TodoList');
+var TodoForm = require('./components/TodoForm');
 
 var TodoApp = React.createClass({
   render: function() {
     return (
     	<div className="todoBox">
         <TodoList />
+        <TodoForm />
       </div>
     );
   }
---
+++ app/scripts/components/TodoForm.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var React = require('react');
+
+var TodoForm = React.createClass({
+	render: function() {
+		return (
+			<div className="todoForm">
+        Hello, world! I am a TodoForm.
+      </div>
+		);
+	}
+});
+
+module.exports = TodoForm;
