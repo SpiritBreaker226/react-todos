6420abc04ddedc8f871c7e576f9af5b3f18d6343
Jason Stathopulos <jstathopulos@gmail.com>
2015-06-23 12:08:24 -0400

    Remove TodoList From Main Component Not Need Here

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,15 +2,6 @@
 var React = window.React = require('react'),
     mountNode = document.getElementById("app");
 
-var TodoList = React.createClass({
-  render: function() {
-    var createItem = function(itemText) {
-      return <li>{itemText}</li>;
-    };
-    return <ul>{this.props.items.map(createItem)}</ul>;
-  }
-});
-
 var TodoApp = React.createClass({
   render: function() {
     return (
