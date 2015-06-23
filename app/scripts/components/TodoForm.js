ca0e06d4f6afe9431e5b9fe525215d1137da56c7
Jason Stathopulos <jstathopulos@gmail.com>
2015-06-23 15:48:20 -0400

    Add Submit Function To Add Items Into Items
    Add Submit function to add items into items state in Main component thus
    allowing the user to add Todo items.

--- app/scripts/components/TodoForm.js
+++ app/scripts/components/TodoForm.js
@@ -3,10 +3,25 @@
 var React = require('react');
 
 var TodoForm = React.createClass({
+	_onSubmit: Function(e) {
+		e.preventDefault();
+
+		var text = React.findDOMNode(this.refs.text).value.trim();
+
+		if(!text) {
+			return;
+		}
+
+		// TODO: send request to TODOApp
+
+		React.findDOMNode(this.refs.text).value = '';
+
+		return;
+	},
 	render: function() {
 		return (
-			<form className='todoForm'>
-				<input type='text' ref='item' />
+			<form className='todoForm' onSubmit={this._onSubmit}>
+				<input type='text' ref='text' />
 				<input type='submit' value='Add' />
 			</form>
 		);
